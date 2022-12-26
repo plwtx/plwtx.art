fbe1b0b17953742bab585c147b3794f3af62c56c
plwtx <h.lenny933@gmail.com>
2022-12-26 11:27:57 +0400

    PluWersion | v0.1

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+<link href="/dist/output.css" rel="stylesheet"></link>;
+
+function App() {
+  return (
+    <div className="">
+      <section className=" min-h-screen bg-len-Bluwska flex justify-center items-center">
+        <h1 className="text-len-Reink text-9xl">PLWTX</h1>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        nsl: ["Noto Sans Lao", "sans-serif"],
+      },
+      colors: {
+        // #FFEF77   Yelow *Corn
+        // #FF0066   Reink *Paradise Pink
+        // #0033FF   Bluwska *Blue RYB
+        // #CFEACE   Greey *Nyanza
+
+        len: {
+          Yelow: "#FFEF77",
+          Reink: "#FF0066",
+          Bluwska: "#0033FF",
+          Greey: "CFEACE",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
