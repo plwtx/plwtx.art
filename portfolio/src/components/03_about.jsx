906a99a8d0ad35c366f79b79565e537a9ff89a33
Len Hera <100115737+plwtx@users.noreply.github.com>
2023-03-27 23:59:59 +0400

    v2.4 | Minor Fix

--- portfolio/src/components/03_about.jsx
+++ portfolio/src/components/03_about.jsx
@@ -7,18 +7,20 @@ export default function About() {
       <div className="grid h-screen place-items-center">
         <div className="min-h-[23rem] min-w-[45rem] flex bg-len-White">
           <img className="max-h-[23rem]" src={pReal} alt="1st Drawing" />
-          <div>
-            <h1 className="text-len-Black text-[1.3rem] font-LineR">
+          <div className="m-6">
+            <h1 className="text-len-Black text-[1.75rem] font-LineR">
               user_00.01.0002.0000003.9
             </h1>
-            <p className=" text-left subpixel-antialiasedfont-nsl font-normal tracking-wide p-3 w-[50rem] text-xl">
-              4D6574206D65206174207468652030782073746174696F6E2E20492
-              07761732074616B656E20746F206120307820492067656E75696E65
-              6C7920776173206F7074696D697374696320616E642062656C69657
-              66564207468697320636F756C6420776F726B2E20476F6420492077
-              6973682049207761736E277420736F207374757069642E204920646
-              964206E6F74207265616C697A65206661737420656E6F7567682077
-              686174207761732068617070656E696E672E20307820506C656173652E
+            <p className="text-left subpixel-antialiasedfont-nsl font-normal tracking-wide w-[34rem] text-md">
+              4D 65 74 20 6D 65 20 61 74 20 74 68 65 20 30 78 20 73 74 61 74 69
+              6F 6E 2E 20 49 20 77 61 73 20 74 61 6B 65 6E 20 74 6F 20 61 20 30
+              78 20 49 20 67 65 6E 75 69 6E 65 6C 79 20 77 61 73 20 6F 70 74 69
+              6D 69 73 74 69 63 20 61 6E 64 20 62 65 6C 69 65 76 65 64 20 74 68
+              69 73 20 63 6F 75 6C 64 20 77 6F 72 6B 2E 20 47 6F 64 20 49 20 77
+              69 73 68 20 49 20 77 61 73 6E 27 74 20 73 6F 20 73 74 75 70 69 64
+              2E 20 49 20 64 69 64 20 6E 6F 74 20 72 65 61 6C 69 7A 65 20 66 61
+              73 74 20 65 6E 6F 75 67 68 20 77 68 61 74 20 77 61 73 20 68 61 70
+              70 65 6E 69 6E 67 2E 20 30 78 20 50 6C 65 61 73 65 2E
             </p>
           </div>
         </div>
