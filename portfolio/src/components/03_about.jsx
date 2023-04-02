a42e1ba0ba92c04bc5a49632a9ed79daf18a4898
Len Hera <100115737+plwtx@users.noreply.github.com>
2023-04-02 18:57:11 +0400

    v2.8 |Daily Fixes / Updates

--- portfolio/src/components/03_about.jsx
+++ portfolio/src/components/03_about.jsx
@@ -1,5 +1,5 @@
 import "../App.css";
-// import Yuuki from "../img/pReal.png";
+import Yuuki from "../img/pReal.png";
 // import Znelka from "../img/pFake.png";
 import chnk from "../img/chnk.png";
 import QRCode from "../img/QRC.png";
@@ -10,21 +10,45 @@ export default function About() {
       <div className=" absolute right-0 top-0 h-[55%] w-[15%] p-3 m-6 flex flex-col"></div>
       <div className="grid h-screen place-items-center">
         <div className="relative w-[40%] h-[35%] bg-len-White flex">
-          <img className="object-fill" src={chnk} alt="3rd Member" />
+          <img className="object-fill" src={Yuuki} alt="3rd Member" />
 
+          {/* QR Code Inside the info card */}
+          <div>
+            <img
+              className="absolute top-0 right-0 pointer-events-none h-[14%] p-1"
+              src={chnk}
+              alt="Profile"
+            />
+          </div>
           {/* About Len */}
           <div className="flex flex-col justify-between flex-shrink-1 p-3">
-            <h1 className="text-len-Black font-UIJP text-3xl">⩇⩇:⩇⩇ 海沙</h1>
-            <p>
-              照顧病人很重要，醫生會跟進，但這是一個充滿痛苦和痛苦的時期。
-              就最小的細節而言，任何人都不應從事任何一種工作，除非他從中得到一些好處。
-              不要在痛斥中生氣在快感中痛斥他要從痛中發一毛希望沒有滋生。
-              除非他們被慾望蒙蔽了雙眼，否則他們不會出來，他們有責任放棄他們的職責，靈魂軟化，那就是辛勞
+            <h1 className="text-len-Black font-LineT text-3xl">紺野木綿季</h1>
+            <p className="text-len-Black font-LineR text-sm">
+              You should call me Len (althogh it is not my official name). A
+              scholar who specializes in the academic study of computer science.
+              I'm interested in web/software development. I'm currently trying
+              my best to continue my education abroad.
             </p>
-            <p>她有一顆充滿愛的心</p>
+            {/* <div>
+              <h3 className="text-len-Black text-xl">Experience:</h3>
+              <li>React.JS</li>
+              <li>TailwindCSS</li>
+              <li>JavaScript</li>
+              <li>HTML / CSS</li>
+            </div>
+            <div>
+              <h3 className="text-len-Black text-xl">Linguistics:</h3>
+              <li>English</li>
+              <li>Turkish</li>
+              <li>Russian</li>
+              <li>German</li>
+              <li>Japanese</li>
+              <p>i like Languages =^･-･^=</p>
+            </div> */}
+            <p className="text-md font-LineT ">=^･-･^= | 0.0.0.000.000.000</p>
           </div>
+          {/* QR Code Inside the info card */}
           <div>
-            {/* QR Code Inside the info card */}
             <img
               className="absolute bottom-0 right-0 pointer-events-none h-[25%] p-1"
               src={QRCode}
