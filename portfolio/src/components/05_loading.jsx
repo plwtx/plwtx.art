27e20778958d7f1110b05be140cdefb29183ec36
Len Hera <100115737+plwtx@users.noreply.github.com>
2023-07-20 01:16:44 +0300

    v3.19 | Minor Fixes

--- portfolio/src/components/05_loading.jsx
+++ portfolio/src/components/05_loading.jsx
@@ -7,11 +7,13 @@ export default function Loading() {
     <div id="Loading" className="snap-start relative min-h-screen bg-len-Black">
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <img
-          className="animate-spin-slow w-[15rem] -z-20"
+          className="animate-spin w-[15rem] -z-20"
           src={stikr}
           alt="7th Drawing"
         />
-        <p className="text-len-White text-center z-50">読み込み中 / Loading</p>
+        <p className="text-len-White bg-len-Black text-center z-50">
+          読み込み中 / Loading
+        </p>
       </div>
     </div>
   );
