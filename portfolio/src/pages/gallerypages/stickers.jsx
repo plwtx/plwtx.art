4e2d2e739cc9a5dc58241ae8ca5a3e30357c897e
Len Hera <100115737+plwtx@users.noreply.github.com>
2023-08-24 18:35:30 +0200

    v3.28 | Sticker's Page update

--- portfolio/src/pages/gallerypages/stickers.jsx
+++ portfolio/src/pages/gallerypages/stickers.jsx
@@ -6,7 +6,15 @@ function Stickers() {
     <div className="snap-y snap-mandatory scroll-smooth h-screen overflow-scroll overflow-x-hidden">
       <div className="snap-start relative min-h-screen bg-len-Black">
         {/* Code */}
-        <h1 className=" text-len-White">Stickers</h1>
+        <div className="poscenter m-3 p-3">
+          <h1 className=" text-len-Black bg-white text-center p-2 m-3 font-NJPBold skew-x-6">
+            Stickers
+          </h1>
+          <p className="bg-white text-len-Black p-6 m-3">
+            Stickers are currently unavailable but once they are they will be
+            displayed here.
+          </p>
+        </div>
       </div>
     </div>
   );
